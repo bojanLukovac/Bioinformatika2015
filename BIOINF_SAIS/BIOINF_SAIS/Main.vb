c0296a44eaaa4d73e5660890bd872e64c023bb65
ivcelic <iris.celic@yahoo.com>
2015-01-13 11:30:56 +0100

    First commit weee

---
+++ BIOINF_SAIS/BIOINF_SAIS/Main.vb
@@ -0,0 +1,61 @@
+﻿Module Main
+
+    Sub Main(args As String())
+
+        'cmd usage: main.exe filename.txt
+        'Main calls the function SAIS(S,SA,n) where S is an input string, SA is an output array and n is the length of the input string
+        If (args.Count <> 1) Then
+            Console.WriteLine("You must specify one input file.")
+            Console.WriteLine("Press enter to terminate..")
+            Console.Read()
+            Return
+        End If
+
+        Dim filename As String = args(0)
+
+        Dim fs As System.IO.FileStream = New IO.FileStream(filename, IO.FileMode.Open, IO.FileAccess.Read)
+
+        Console.WriteLine(filename + ": {0} bytes", fs.Length)
+
+        'declare S as input string
+        Dim S(fs.Length - 1) As Byte
+
+        'declare SA array as output suffix array of S
+        Dim SA(fs.Length - 1) As Integer
+
+        fs.Read(S, 0, S.Length)
+
+        'check if input string exists
+        If (IsNothing(S) Or IsNothing(SA) Or (S.Length = 0)) Then
+            Console.WriteLine("An error occured. Check your input string.")
+            Console.WriteLine("Press enter to terminate...")
+            Console.Read()
+            Return
+        End If
+
+
+        Dim sw As System.Diagnostics.Stopwatch = New System.Diagnostics.Stopwatch()
+
+        sw.Start()
+        'starts the SAIS algorithm
+        SAIS.Evaluate(S, SA, S.Length)
+        sw.Stop()
+
+        'write on output the final result - SA 
+        Console.Write("SA= ")
+        For Each i As Integer In SA
+            Console.Write("{0} ", i)
+        Next
+        Console.WriteLine()
+
+        Dim elapsedTime As Double = sw.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency
+        Console.WriteLine("Elapsed time: {0:f8}s", elapsedTime)
+
+        'check(T, SA, T.Length, true)
+        S = Nothing
+        SA = Nothing
+
+        Console.Read()
+    End Sub
+
+End Module
---
+++ BIOINF_SAIS/BIOINF_SAIS/SAIS.vb
@@ -0,0 +1,151 @@
+﻿Public Class SAIS
+
+    Const MINBUCKETSIZE As Integer = 256
+    Shared Sub Evaluate(S As Byte(), SA As Integer(), n As Integer)
+
+        'check if input string length is 1, return SA
+        If (n = 1) Then
+            SA(0) = 0
+            Return
+        End If
+
+        sais_main(New ByteArray(S, 0), SA, 0, n, MINBUCKETSIZE, False)
+
+        Return
+
+
+    End Sub
+
+    Private Shared Sub sais_main(ByVal S As ByteArray, ByVal sa As Integer(), ByVal fs As Integer, ByVal n As Integer, ByVal k As Integer, ByVal isbwt As Boolean)
+
+        Dim C, B, RA As IArray
+        Dim flags As Integer
+
+        If (k <= MINBUCKETSIZE) Then
+            C = New IntArray(New Integer(k) {}, 0)
+            If (k <= fs) Then
+                B = New IntArray(sa, n + fs - k)
+                flags = 1
+            Else
+                B = New IntArray(New Integer(k) {}, 0)
+                flags = 3
+            End If
+
+        ElseIf (k <= fs) Then
+
+
+        Else
+
+        End If
+    End Sub
+End Class
+
+Friend Interface IArray
+    Default Property Item(i As Integer) As Integer
+
+End Interface
+
+Friend Class ByteArray
+    Implements IArray
+    Private m_array As Byte()
+    Private m_pos As Integer
+    Public Sub New(array As Byte(), pos As Integer)
+        m_pos = pos
+        m_array = array
+    End Sub
+    Protected Shadows Sub Finalize()
+        Try
+            m_array = Nothing
+        Finally
+            MyBase.Finalize()
+        End Try
+    End Sub
+    Default Public Property Item(i As Integer) As Integer Implements IArray.Item
+        Get
+            Return CInt(m_array(i + m_pos))
+        End Get
+        Set(value As Integer)
+            m_array(i + m_pos) = CByte(value)
+        End Set
+    End Property
+
+End Class
+
+Friend Class CharArray
+    Implements IArray
+    Private m_array As Char()
+    Private m_pos As Integer
+    Public Sub New(array As Char(), pos As Integer)
+        m_pos = pos
+        m_array = array
+    End Sub
+    Protected Shadows Sub Finalize()
+        Try
+            m_array = Nothing
+        Finally
+            MyBase.Finalize()
+        End Try
+    End Sub
+    Default Public Property Item(i As Integer) As Integer Implements IArray.Item
+        Get
+            Return AscW(m_array(i + m_pos))
+        End Get
+        Set(value As Integer)
+            m_array(i + m_pos) = ChrW(value)
+        End Set
+    End Property
+End Class
+
+Friend Class IntArray
+    Implements IArray
+    Private m_array As Integer()
+    Private m_pos As Integer
+    Public Sub New(array As Integer(), pos As Integer)
+        m_pos = pos
+        m_array = array
+    End Sub
+    Protected Overrides Sub Finalize()
+        Try
+            m_array = Nothing
+        Finally
+            MyBase.Finalize()
+        End Try
+    End Sub
+    Default Public Property Item(i As Integer) As Integer Implements IArray.Item
+        Get
+            Return m_array(i + m_pos)
+        End Get
+        Set(value As Integer)
+            m_array(i + m_pos) = value
+        End Set
+    End Property
+End Class
+
+Friend Class StringArray
+    Implements IArray
+    Private m_array As String
+    Private m_pos As Integer
+    Public Sub New(array As String, pos As Integer)
+        m_pos = pos
+        m_array = array
+    End Sub
+    Protected Overrides Sub Finalize()
+        Try
+            m_array = Nothing
+        Finally
+            MyBase.Finalize()
+        End Try
+    End Sub
+    Default Public Property Item(i As Integer) As Integer Implements IArray.Item
+        Get
+            Return AscW(m_array(i + m_pos))
+        End Get
+        Set(value As Integer)
+        End Set
+    End Property
+End Class
+
+
+
+
+
