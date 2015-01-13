91d64023e9a3275042363f583761a9f1c13499c4
paulapetra <paulapetra.mic@gmail.com>
2015-01-13 16:44:22 +0100

    added check function

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 #include <stdlib.h>
 #include <time.h>
 
+//compares SA and input string
+//to see if SA is correctly sorted
+int checkSort(unsigned char *str, int *SA, int n){
+int i,j,l,str1,str2,flag;
+flag=1;	
+ for(i = 0;  i < n-1;  i++) {
+	if(SA[i]>SA[i+1]) 
+		l=n-SA[i];
+	else 
+		l=n-SA[i+1];	
+	str1=SA[i];
+	str2=SA[i+1];
+	for(j=0;j<l;j++){
+		if (str[str1+j]<str[str2+j]){
+			flag=1;
+			break;
+		}
+		else if (str[str1+j]>str[str2+j]){
+			flag=0;
+		}
+	}
+}
+return flag;
+}
 
 void sais(unsigned char *str, int *SA, int n, int m, int size);
 
