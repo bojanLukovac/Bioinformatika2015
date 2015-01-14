87b9a15191d684101e480760d51e91922d734956
Goran Hrzenjak <ghoc.hrx@gmail.com>
2015-01-14 12:10:53 +0100

    Added class SAIS

--- GoranHrzenjak_Ruby/main.rb
+++ GoranHrzenjak_Ruby/main.rb
@@ -4,8 +4,22 @@
 $LOAD_PATH << '.'
 
 require "parser"
-
+require "helpers"
+require "sais_methods"
 
 
 input_parser = Parser.new
 input_parser.parse_input_args
+
+input_parser.get_input_strings.each do |input_string|
+  #puts input_string
+  #puts input_string.length
+  
+  #convert to ASCII values
+
+  input_string_ASCII = input_string.unpack("c*")
+  
+  sa_is = SAIS.new(input_string_ASCII)
+  
+  
+end
---
+++ GoranHrzenjak_Ruby/sais_methods.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class SAIS
+  
+  def initialize(input_string)
+    @original_input_string = input_string
+    @recursion_level = 0
+  end
+  
+end
