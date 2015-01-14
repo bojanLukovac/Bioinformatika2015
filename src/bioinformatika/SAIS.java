c7e946152daa6b307daab40b9737e2ba23327dc2
drago.crnjac <drago.crnjac@gmail.com>
2015-01-14 14:32:21 +0100

    Extracted some functionality to new class.

--- src/bioinformatika/SAIS.java
+++ src/bioinformatika/SAIS.java
@@ -3,7 +3,6 @@ package bioinformatika;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
@@ -11,47 +10,81 @@ public class SAIS {
 
 	public static void main(String[] args) {
 		
-		String firstSolution = doSAIS("ATTAGCGAGCG$");
-		String secondSolution = doSAIS(firstSolution);
+		ArrayList<SaisStep> saisSteps = new ArrayList<>();
+		String input = "ATTAGCGAGCG$";
 		
-		System.out.println("FIRST: " + firstSolution);
-		System.out.println("SECOND: " + secondSolution);
+		// Do SA-IS steps until we reach array with all different names
+		while (true) {
+			SaisStep solution = doSAIS(input);
+			saisSteps.add(solution);
+			
+			if (areAllElementsDifferent(solution.getArrayWithNewNames()) == false) {
+				input = solution.joinArray(solution.getArrayWithNewNames());
+			} else {
+				System.out.println("All are different!");
+				break;
+			}
+		}
+		
+		
+		/*
+		System.out.println("PRINTING IT");
+		for (SaisStep step : saisSteps) {
+			System.out.println(step.joinArray(step.getArrayWithNewNames()));
+		}
+		*/
+		
+		/*
+		SaisStep firstSolution = doSAIS("ATTAGCGAGCG$");
+		SaisStep secondSolution = doSAIS(firstSolution.joinArray(firstSolution.getArrayWithNewNames()));
+		
+		System.out.println("FIRST: " + firstSolution.joinArray(firstSolution.getArrayWithNewNames()));
+		System.out.println("SECOND: " + secondSolution.joinArray(secondSolution.getArrayWithNewNames()));
+		*/
 		
 	}
 	
+	/*
+	 * Returns true if all elements in array are different	
+	 * */
+	public static boolean areAllElementsDifferent(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] == array[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 	
-	public static String doSAIS(String inputString) {
+	public static SaisStep doSAIS(String inputString) {
 
 		String input = inputString;
-		
 		String[] S = input.split("(?!^)");
 		
-
-		System.out.println(Arrays.toString(S));
-
-		boolean[] t = getT(S);
-		System.out.println(Arrays.toString(t));
+		SaisStep saisStep = new SaisStep();
+		saisStep.setS(S);
 		
-		ArrayList<Integer> lmsPointersArray = getLMSPointersArray(S, t);
-			
-		System.out.println(lmsPointersArray);
+		boolean[] t = saisStep.getTFromS(S);
+		saisStep.setT(t);
+		ArrayList<Integer> lmsPointersArray = saisStep.getLMSPointersArray(S, t);
+		saisStep.setLmsPointersArray(lmsPointersArray);
+		
+		System.out.println(Arrays.toString(saisStep.getS()));
+		System.out.println(Arrays.toString(saisStep.getT()));
+		System.out.println(saisStep.getLmsPointersArray());
 		
 		
 		/*
 		 * Create SA buckets and insert -1 for each value
 		 * */
-		TreeMap<String, ArrayList<Integer>> bucketHash = new TreeMap<>();
- 		for (int i = 0; i < S.length; i++) {
- 			if (bucketHash.containsKey(S[i])) {
- 				bucketHash.get(S[i]).add(-1);
- 			} else {
- 				ArrayList<Integer> values = new ArrayList<>();
- 				values.add(-1);
- 				bucketHash.put(S[i], values);
- 			}
-		}
-
+		TreeMap<String, ArrayList<Integer>> bucketHash = saisStep.getBucketHashFromSAndInitializeToMinusOne(S);
+		saisStep.setBucketHash(bucketHash);
+		
  		Object[] keySet = bucketHash.keySet().toArray();
+ 		saisStep.setKeySet(keySet);
+ 		
  		System.out.println(Arrays.toString(keySet));
  		
  		/*
@@ -61,7 +94,8 @@ public class SAIS {
  		 *	We do the similar thing here :)
  		 * */
  		HashMap<String, Integer> bucketPointers = new HashMap<>();
- 		setAllPointersInBucketsToEnd(keySet, bucketPointers, bucketHash);
+ 		saisStep.setBucketPointers(bucketPointers);
+ 		saisStep.setAllPointersInBucketsToEnd(keySet, bucketPointers, bucketHash);
  		
  		/*
  		 * Algorithm 1, first iteration
@@ -81,42 +115,23 @@ public class SAIS {
  	    }
  		
  		/*
- 		 * Algorithm 2, second iteration
+ 		 * Algorithm 1, second iteration
+ 		 * Set all pointers in buckets to beginning
  		 * */
- 		//Set all pointers in buckets to beginning
- 		setAllPointersInBucketsToBeginning(keySet, bucketPointers);
- 		
- 		// Iterate over all buckets
- 		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
- 	        for (Integer integerInBucket : entry.getValue()) {
- 	        	if (integerInBucket > 0 && t[integerInBucket - 1] == true) {
- 	        		bucketHash.get(S[integerInBucket - 1]).set(bucketPointers.get(S[integerInBucket - 1]), integerInBucket - 1);
- 	        		bucketPointers.put(S[integerInBucket - 1], bucketPointers.get(S[integerInBucket - 1]) + 1);		
- 	        	}
- 	        }
- 	    }
-
+ 		saisStep.setAllPointersInBucketsToBeginning(keySet, bucketPointers);
+ 		saisStep.doSecondIteration(S, t, bucketHash, bucketPointers);
  		System.out.println("____Second iteration______");
  		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
  	        System.out.println("key ->" + entry.getKey() + ", value->" + entry.getValue());   
  	    }
  		
- 		/*
- 		 * Algorithm 3, third iteration
- 		 * */
- 		setAllPointersInBucketsToEnd(keySet, bucketPointers, bucketHash);
  		
- 		// Iterate from the end to the beginning
- 		for (int i = keySet.length - 1; i >= 0; i--) { 	        
- 	        for (int j = bucketHash.get(keySet[i]).size() - 1; j >= 0; j--) {
- 	        	int integerInBucket = bucketHash.get(keySet[i]).get(j);
-				if (integerInBucket > 0 && t[integerInBucket - 1] == false) {
-					bucketHash.get(S[integerInBucket - 1]).set(bucketPointers.get(S[integerInBucket - 1]), integerInBucket - 1);
-					bucketPointers.put(S[integerInBucket - 1], bucketPointers.get(S[integerInBucket - 1]) - 1);
-				}
-			}
- 	    }
-
+ 		/*
+ 		 * Algorithm 1, third iteration
+ 		 * Set all pointers in buckets to end
+ 		 * */
+ 		saisStep.setAllPointersInBucketsToEnd(keySet, bucketPointers, bucketHash);
+ 		saisStep.doThirdIteration(S, t, bucketHash, bucketPointers, keySet);
  		System.out.println("_____Third iteration__________");
  		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
  	        System.out.println("key ->" + entry.getKey() + ", value->" + entry.getValue());   
@@ -127,171 +142,14 @@ public class SAIS {
  		// boolean result = compareLMSSubstrings(3, 7, S, t);
  		 //System.out.println(result);
  		
- 		// STEP 4,  Give new names to each LMS substring
- 		int[] S1 = new int[lmsPointersArray.size()];
- 		ArrayList<Integer> SA = new ArrayList<>();
- 	
-		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
-			for (Integer integerInBucket : entry.getValue()) {
-				SA.add(integerInBucket);
-			}
-		}
-	
-		int newName = 0;
-		
-		int lastKnownIndexOfLMSSubstringInSA = 0;
-		for (int i = 0; i < SA.size(); i++) {
-			if (lmsPointersArray.contains(SA.get(i))) {
-				lastKnownIndexOfLMSSubstringInSA = i;
-				System.out.println(lastKnownIndexOfLMSSubstringInSA);
-				break;
-			}
-		}
-		S1[lastKnownIndexOfLMSSubstringInSA] = newName;
-		
-		for (int i = lastKnownIndexOfLMSSubstringInSA + 1; i < SA.size(); i++) {
-			if (lmsPointersArray.contains(SA.get(i)) == true) {
-				
-				if(compareLMSSubstrings(SA.get(i), SA.get(lastKnownIndexOfLMSSubstringInSA), S, t) == true) {
-					S1[lmsPointersArray.indexOf(SA.get(i))] = newName;
-				} else {
-					newName++;
-					S1[lmsPointersArray.indexOf(SA.get(i))] = newName;
-				}
-				lastKnownIndexOfLMSSubstringInSA = i;
-			} 
-		}
-
+ 		// STEP 4,  Get new array with new name for each LMS substring
+ 		int[] S1 = saisStep.getArrayWithNewNamesForEachLMSSubstring(lmsPointersArray, bucketHash, S, t);
+ 		saisStep.setArrayWithNewNames(S1);
 		System.out.println(Arrays.toString(S1));
 		
-		return joinArray(S1);
+		return saisStep;
 	
 	}
 	
-	public static String joinArray(int[] aArr) {
-	    StringBuilder sbStr = new StringBuilder();
-	    for (int i = 0, il = aArr.length; i < il; i++) {
-	        sbStr.append(aArr[i]);
-	    }
-	    return sbStr.toString();
-	}
-	
-	public static boolean compareLMSSubstrings(int firstLMS, int secondLMS, String[] S, boolean[] t) {
-		boolean isFirstIteration = true;
-		while (S[firstLMS].equals(S[secondLMS])) {
-
-			// Do only once
-			if (isFirstIteration == true) {
-				if (t[firstLMS] == false && t[secondLMS] == false) {
-					firstLMS++;
-					secondLMS++;
-					// Check if we're comparing last lms strings
-					if (firstLMS == secondLMS && firstLMS >= S.length) {
-						return true;
-					}
-					isFirstIteration = false;
-					continue;
-				}
-			}
-			
-			
-			if (t[firstLMS] == false && t[secondLMS] == false) {
-				return true;
-			} else if (t[firstLMS] == true && t[secondLMS] == true) {
-				firstLMS++;
-				secondLMS++;
-			} else {
-				return false;
-			}
-		
-		}
-		
-		return false;
-		
-	}
-	
-	public static void setAllPointersInBucketsToBeginning(Object[] keySet, HashMap<String, Integer> bucketPointers) {
-		for (int i = 0; i < keySet.length; i++) {
-			bucketPointers.put((String) keySet[i], 0);
-		}
-	}
-	
-	public static void setAllPointersInBucketsToEnd(Object[] keySet, HashMap<String, Integer> bucketPointers, TreeMap<String, ArrayList<Integer>> bucketHash) {
-		for (int i = 0; i < keySet.length; i++) {
-			bucketPointers.put((String) keySet[i], bucketHash.get((String) keySet[i]).size() - 1);
-		}
-	}
-	
-	/*	Returns array of booleans that tells us the types of characters
-	 * 	L = true
-	 * 	S = false
-	 * */
-	public static boolean[] getT(String[] S) {
-		
-		boolean[] t = new boolean[S.length];
-		t[S.length - 1] = false;
-		t[S.length - 2] = true;
-		
-		for (int i = 0; i < S.length - 2; i++) {
-			int comparation = S[i].compareTo(S[i + 1]); 
-
-			if (comparation > 0) {
-				t[i] = true;
-			} else if (comparation < 0) {
-				t[i] = false;
-			} else {
-				int j = i + 1;
-				
-				while (S[j].compareTo(S[j + 1]) == 0) {
-					j++;
-				}
-
-				if (S[j].compareTo(S[j + 1]) > 0) {
-					setArray(t, i, j, true);
-				} else {
-					setArray(t, i, j, true);
-				}
-				i = j;
-			}
-		}
-		return t;
-	}
-
-	/*
-	 * 	Set array from "i" to "j" to booleanValue
-	 * 
-	 * */
-	private static void setArray(boolean[] t, int i, int j, boolean booleanValue) {
-		for (; i <= j; i++) {
-			t[i] = booleanValue;
-		}
-	}
-
-	/*
-	 * Returns array holding pointers to LMS-substrings
-	 * String[] S is the input
-	 * boolean[] t keeps track of S-types and L-types
-	 * S type = false
-	 * L type = true
-	 * */
-	public static ArrayList<Integer> getLMSPointersArray(String[] S, boolean[] t) {
-		ArrayList<Integer> lmsPointers = new ArrayList<>();
-		for (int i = 1; i < S.length; i++) {
-			if (t[i - 1] == true && t[i] == false) {
-				lmsPointers.add(i);
-			}
-		}
-		return lmsPointers;
-	}
-	
-	
-	/*
-	 * Class that's used for "bucket" part of the algorithm
-	 * */
-	public static class Bucket {
-		
-		
-		
-	}
  	
 }
---
+++ src/bioinformatika/SaisStep.java
@@ -0,0 +1,308 @@
+package bioinformatika;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class SaisStep {
+
+	private String[] S;
+	private boolean[] t;
+	private ArrayList<Integer> lmsPointersArray;
+	private TreeMap<String, ArrayList<Integer>> bucketHash;
+	private Object[] keySet;
+	private HashMap<String, Integer> bucketPointers;
+	private int[] arrayWithNewNames;
+	
+	public SaisStep(String[] s, boolean[] t,
+			ArrayList<Integer> lmsPointersArray,
+			TreeMap<String, ArrayList<Integer>> bucketHash, Object[] keySet,
+			HashMap<String, Integer> bucketPointers, int[] arrayWithNewNames) {
+		super();
+		this.S = s;
+		this.t = t;
+		this.lmsPointersArray = lmsPointersArray;
+		this.bucketHash = bucketHash;
+		this.keySet = keySet;
+		this.bucketPointers = bucketPointers;
+		this.arrayWithNewNames = arrayWithNewNames;
+	}
+
+	public SaisStep() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int[] getArrayWithNewNames() {
+		return arrayWithNewNames;
+	}
+
+	public void setArrayWithNewNames(int[] arrayWithNewNames) {
+		this.arrayWithNewNames = arrayWithNewNames;
+	}
+
+	public String[] getS() {
+		return S;
+	}
+
+	public void setS(String[] s) {
+		S = s;
+	}
+
+	public boolean[] getT() {
+		return t;
+	}
+
+	public void setT(boolean[] t) {
+		this.t = t;
+	}
+
+	public ArrayList<Integer> getLmsPointersArray() {
+		return lmsPointersArray;
+	}
+
+	public void setLmsPointersArray(ArrayList<Integer> lmsPointersArray) {
+		this.lmsPointersArray = lmsPointersArray;
+	}
+
+	public TreeMap<String, ArrayList<Integer>> getBucketHash() {
+		return bucketHash;
+	}
+
+	public void setBucketHash(TreeMap<String, ArrayList<Integer>> bucketHash) {
+		this.bucketHash = bucketHash;
+	}
+
+	public Object[] getKeySet() {
+		return keySet;
+	}
+
+	public void setKeySet(Object[] keySet) {
+		this.keySet = keySet;
+	}
+
+	public HashMap<String, Integer> getBucketPointers() {
+		return bucketPointers;
+	}
+
+	public void setBucketPointers(HashMap<String, Integer> bucketPointers) {
+		this.bucketPointers = bucketPointers;
+	}
+
+
+	public int[] getArrayWithNewNamesForEachLMSSubstring(ArrayList<Integer> lmsPointersArray,
+			TreeMap<String, ArrayList<Integer>> bucketHash, String[] S, boolean[] t) {
+	
+		int[] S1 = new int[lmsPointersArray.size()];
+ 		ArrayList<Integer> SA = new ArrayList<>();
+ 	
+		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
+			for (Integer integerInBucket : entry.getValue()) {
+				SA.add(integerInBucket);
+			}
+		}
+	
+		int newName = 0;
+		
+		int lastKnownIndexOfLMSSubstringInSA = 0;
+		for (int i = 0; i < SA.size(); i++) {
+			if (lmsPointersArray.contains(SA.get(i))) {
+				lastKnownIndexOfLMSSubstringInSA = i;
+				System.out.println(lastKnownIndexOfLMSSubstringInSA);
+				break;
+			}
+		}
+		S1[lastKnownIndexOfLMSSubstringInSA] = newName;
+		
+		for (int i = lastKnownIndexOfLMSSubstringInSA + 1; i < SA.size(); i++) {
+			if (lmsPointersArray.contains(SA.get(i)) == true) {
+				
+				if(compareLMSSubstrings(SA.get(i), SA.get(lastKnownIndexOfLMSSubstringInSA), S, t) == true) {
+					S1[lmsPointersArray.indexOf(SA.get(i))] = newName;
+				} else {
+					newName++;
+					S1[lmsPointersArray.indexOf(SA.get(i))] = newName;
+				}
+				lastKnownIndexOfLMSSubstringInSA = i;
+			} 
+		}
+
+		return S1;
+	
+	}
+
+
+	public void doThirdIteration(String[] S, boolean[] t,
+			TreeMap<String, ArrayList<Integer>> bucketHash,
+			HashMap<String, Integer> bucketPointers, Object[] keySet) {
+		
+		// Iterate from the end to the beginning
+		for (int i = keySet.length - 1; i >= 0; i--) {
+			for (int j = bucketHash.get(keySet[i]).size() - 1; j >= 0; j--) {
+				int integerInBucket = bucketHash.get(keySet[i]).get(j);
+				if (integerInBucket > 0 && t[integerInBucket - 1] == false) {
+					bucketHash.get(S[integerInBucket - 1]).set(
+							bucketPointers.get(S[integerInBucket - 1]),
+							integerInBucket - 1);
+					bucketPointers.put(S[integerInBucket - 1],
+							bucketPointers.get(S[integerInBucket - 1]) - 1);
+				}
+			}
+		}
+		
+	}
+
+
+	public void doSecondIteration(String[] S, boolean[] t, TreeMap<String, ArrayList<Integer>> bucketHash, HashMap<String, Integer> bucketPointers) {
+		// Iterate over all buckets
+		for (Entry<String, ArrayList<Integer>> entry : bucketHash.entrySet()) {
+			for (Integer integerInBucket : entry.getValue()) {
+				if (integerInBucket > 0 && t[integerInBucket - 1] == true) {
+					bucketHash.get(S[integerInBucket - 1]).set(
+							bucketPointers.get(S[integerInBucket - 1]),
+							integerInBucket - 1);
+					bucketPointers.put(S[integerInBucket - 1],
+							bucketPointers.get(S[integerInBucket - 1]) + 1);
+				}
+			}
+		}
+
+	}
+
+
+	/*
+	 * Create SA buckets and insert -1 for each value
+	 * */
+	public TreeMap<String, ArrayList<Integer>> getBucketHashFromSAndInitializeToMinusOne(String[] S) {
+
+		TreeMap<String, ArrayList<Integer>> bucketHash = new TreeMap<>();
+		for (int i = 0; i < S.length; i++) {
+			if (bucketHash.containsKey(S[i])) {
+				bucketHash.get(S[i]).add(-1);
+			} else {
+				ArrayList<Integer> values = new ArrayList<>();
+				values.add(-1);
+				bucketHash.put(S[i], values);
+			}
+		}
+		return bucketHash;
+	}
+
+
+	public String joinArray(int[] aArr) {
+	    StringBuilder sbStr = new StringBuilder();
+	    for (int i = 0, il = aArr.length; i < il; i++) {
+	        sbStr.append(aArr[i]);
+	    }
+	    return sbStr.toString();
+	}
+	
+	public boolean compareLMSSubstrings(int firstLMS, int secondLMS, String[] S, boolean[] t) {
+		boolean isFirstIteration = true;
+		while (S[firstLMS].equals(S[secondLMS])) {
+
+			// Do only once
+			if (isFirstIteration == true) {
+				if (t[firstLMS] == false && t[secondLMS] == false) {
+					firstLMS++;
+					secondLMS++;
+					// Check if we're comparing last lms strings
+					if (firstLMS == secondLMS && firstLMS >= S.length) {
+						return true;
+					}
+					isFirstIteration = false;
+					continue;
+				}
+			}
+			
+			
+			if (t[firstLMS] == false && t[secondLMS] == false) {
+				return true;
+			} else if (t[firstLMS] == true && t[secondLMS] == true) {
+				firstLMS++;
+				secondLMS++;
+			} else {
+				return false;
+			}
+		
+		}
+		
+		return false;
+		
+	}
+	
+	public void setAllPointersInBucketsToBeginning(Object[] keySet, HashMap<String, Integer> bucketPointers) {
+		for (int i = 0; i < keySet.length; i++) {
+			bucketPointers.put((String) keySet[i], 0);
+		}
+	}
+	
+	public void setAllPointersInBucketsToEnd(Object[] keySet, HashMap<String, Integer> bucketPointers, TreeMap<String, ArrayList<Integer>> bucketHash) {
+		for (int i = 0; i < keySet.length; i++) {
+			bucketPointers.put((String) keySet[i], bucketHash.get((String) keySet[i]).size() - 1);
+		}
+	}
+	
+	/*	Returns array of booleans that tells us the types of characters
+	 * 	L = true
+	 * 	S = false
+	 * */
+	public boolean[] getTFromS(String[] S) {
+		
+		boolean[] t = new boolean[S.length];
+		t[S.length - 1] = false;
+		t[S.length - 2] = true;
+		
+		for (int i = 0; i < S.length - 2; i++) {
+			int comparation = S[i].compareTo(S[i + 1]); 
+
+			if (comparation > 0) {
+				t[i] = true;
+			} else if (comparation < 0) {
+				t[i] = false;
+			} else {
+				int j = i + 1;
+				
+				while (S[j].compareTo(S[j + 1]) == 0) {
+					j++;
+				}
+
+				if (S[j].compareTo(S[j + 1]) > 0) {
+					setArray(t, i, j, true);
+				} else {
+					setArray(t, i, j, true);
+				}
+				i = j;
+			}
+		}
+		return t;
+	}
+
+	/*
+	 * 	Set array from "i" to "j" to booleanValue
+	 * 
+	 * */
+	private void setArray(boolean[] t, int i, int j, boolean booleanValue) {
+		for (; i <= j; i++) {
+			t[i] = booleanValue;
+		}
+	}
+
+	/*
+	 * Returns array holding pointers to LMS-substrings
+	 * String[] S is the input
+	 * boolean[] t keeps track of S-types and L-types
+	 * S type = false
+	 * L type = true
+	 * */
+	public ArrayList<Integer> getLMSPointersArray(String[] S, boolean[] t) {
+		ArrayList<Integer> lmsPointers = new ArrayList<>();
+		for (int i = 1; i < S.length; i++) {
+			if (t[i - 1] == true && t[i] == false) {
+				lmsPointers.add(i);
+			}
+		}
+		return lmsPointers;
+	}
+
+}
